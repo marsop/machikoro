fcc2742651fb3fea5c6e12f08ca8fa8f4932f1d0
marsop <marsop@gmail.com>
2015-09-14 22:16:09 +0200

    Improve game playing behavior

--- code/game.py
+++ code/game.py
@@ -1,32 +1,60 @@
 from card import Card, BuildingCard, VictoryCard, CardType
 from player import Player
 
-def main():
-    
-    player_names = ["Al", "So"]
+player_names = ["Al", "So"]
+
+def initialize_game():
+    # initiate game    
     players = []
     board = []
-    
-    board.append(BuildingCard("bakery", 1, CardType.GREEN, [2,3], 1, False))
-    board.append(BuildingCard("wheat field", 1, CardType.BLUE, [1], 1, False))
-    board.append(BuildingCard("cafe", 2, CardType.RED, [3], 1, False))
-    board.append(VictoryCard("train station", 4, CardType.GOLDEN, False))
-    board.append(VictoryCard("amusement park", 10, CardType.GOLDEN, False))
-    board.append(VictoryCard("card3", 16, CardType.GOLDEN, False))
-    board.append(VictoryCard("card4", 22, CardType.GOLDEN, False))
+            
+    for i in range (0,6):
+        board.append(BuildingCard("bakery", 1, CardType.GREEN, [2,3], 1, False))
+        board.append(BuildingCard("farm", 1, CardType.BLUE, [2], 1, False))
+        board.append(BuildingCard("wheat field", 1, CardType.BLUE, [1], 1, False))
+        board.append(BuildingCard("cafe", 2, CardType.RED, [3], 1, False))
+        board.append(BuildingCard("mini market", 2, CardType.GREEN, [4], 3, False))
+        board.append(BuildingCard("forest", 3, CardType.BLUE, [5], 1, False))
+        board.append(BuildingCard("mine", 6, CardType.BLUE, [9], 5, False))
+        board.append(BuildingCard("milk factory", 5, CardType.GREEN, [7], 0, False))
+        board.append(BuildingCard("furniture factory", 3, CardType.GREEN, [8], 0, False))
+        board.append(BuildingCard("family restaurant", 3, CardType.RED, [9,10], 2, False))
+        board.append(BuildingCard("mall", 2, CardType.GREEN, [11,12], 0, False))
+        board.append(BuildingCard("plantation", 3, CardType.BLUE, [10], 3, False))
     
     for name in player_names:
-    
-        # initiate game
-        bakery = BuildingCard("bakery", 0, CardType.GREEN, [2,3], 1, False)
-        wheat_field = BuildingCard("wheat field", 0, CardType.BLUE, [1], 1, False)
+        board.append(BuildingCard("stadium", 6, CardType.PURPLE, [6], 0, True))
+        board.append(BuildingCard("television tower", 7, CardType.PURPLE, [6], 0, True))
+        board.append(BuildingCard("office building", 8, CardType.PURPLE, [6], 0, True))
 
         player = Player(name)
+        
+        player.add_card(VictoryCard("train station", 4, CardType.GOLDEN, False))
+        player.add_card(VictoryCard("shopping mall", 10, CardType.GOLDEN, False))
+        player.add_card(VictoryCard("amusement park", 16, CardType.GOLDEN, False))
+        player.add_card(VictoryCard("radio tower", 22, CardType.GOLDEN, False))
+        
+        player.buy_card(BuildingCard("bakery", 0, CardType.GREEN, [2,3], 1, False))
+        player.buy_card(BuildingCard("wheat field", 0, CardType.BLUE, [1], 1, False))
+        
         player.take_coins(3)
-        player.buy_card(bakery)
-        player.buy_card(wheat_field)
         
         players.append(player)
+        
+def play_game(): 
+    play_round()
+    
+def play_round():
+    for player in player_names:
+        play_turn(player)
+        
+def play_turn(player):
+    print "playing turn for " + player
+    
+
+def main():
+    initialize_game()
+    play_game()
 
 if __name__ == "__main__":
     main()
--- code/player.py
+++ code/player.py
@@ -9,6 +9,10 @@ class Player:
 		self.playing_area.append(card)
 		print "player " + self.name + " has taken card " + card.title + " (coins left "+ str(self.coins)+")"
 		
+	def add_card(self, card):
+		self.playing_area.append(card)
+		print "player " + self.name + " has taken card " + card.title + " (coins left "+ str(self.coins)+")"
+		
 	def take_coins(self, amount):
 		self.coins += amount
 		print "player " + self.name + " has now " + str(self.coins) + " coins"
